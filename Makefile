32a253bac9760d8b2091742240feedbffbacca2e
bsipiak <bartosz.sipiak@olx.pl>
2019-08-06 10:05:08 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,11 @@
+up:
+	docker-compose up -d
+
+build:
+	docker-compose build
+
+start:
+	docker-compose start
+
+stop:
+	docker-compose stop
---
+++ public_html/config.php
@@ -0,0 +1,5 @@
+<?php
+
+/* Define username and password */
+$Username = 'user';
+$Password = '$2y$10$AVnhKZ/6VpBUglMn3U.3AOMEH4C36v4ro5Ul2T4pZUoX7a2zZTelm';
